6104c1f3bb24b6553848c2962b3792780015587d
Ivan Krause <ivan.krause@gmail.com>
2023-11-15 19:53:32 +0000

    setup static folder and update settings.py

--- lavoro/settings.py
+++ lavoro/settings.py
@@ -159,10 +159,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# Static files (CSS, JavaScript, images)
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Additional locations of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
