2615a91c72612641c3eec872693ab22f3c55ad15
Ivan Krause <ivan.krause@gmail.com>
2023-11-16 20:10:20 +0000

    create basic booking model and add to admin

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+    member = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created = models.DateTimeField(default=datetime.now, blank=True)
