0497d113e1e1beaf13868973354b7f17bcecae4a
Ivan Krause <ivan.krause@gmail.com>
2023-11-23 13:04:00 +0000

    update debug

--- env.py
+++ env.py
@@ -2,4 +2,3 @@ import os
 
 os.environ["SECRET_KEY"] = "3g5g4trr335t35t33g546g4h464h4h53tg3"
 os.environ["DATABASE_URL"] = "postgres://uamvqwjw:pg3aN34k836--HBOgwRpnGshDMysTjKL@flora.db.elephantsql.com/uamvqwjw"
-os.environ["DEBUG_STATUS"] = True
