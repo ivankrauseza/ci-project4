29072028a0b8b9b87714db2cda6c05ff360d570a
Ivan Krause <ivan.krause@gmail.com>
2023-11-23 13:49:26 +0000

    update settings.py debug

--- lavoro/settings.py
+++ lavoro/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['localhost', 'https://ivankrause-ci-project4-acbd33a75ce8.herokuapp.com']
 
